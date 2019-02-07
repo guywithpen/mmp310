3bf47968769788f4542826013d0971fd5fba15ad
guywithpen <abreujaysonschool@gmail.com>
2019-02-06 19:08:29 -0500

    Update sketch.js

--- story/sketch.js
+++ story/sketch.js
@@ -4,7 +4,8 @@
 
 // global variable
 
-
+//beginning, middle, end
+var currentSetting = "beginning";
 
 function setup() {
     createCanvas (windowWidth, windowHeight);
@@ -13,20 +14,39 @@ function setup() {
 function draw() {
     background(50);
     
+    //sky
+    fill('#aec4e5');
+    noStroke();
+    rect(0, 0, 1000, 8000);
+    
     // first character
     fill('#4c7e84');
     noStroke();
-    ellipse(200, 200, 200, 200);    // body
+    ellipse(200, 590, 260, 200);    // body
     stroke(0);
-    ellipse(250, 180, 20);  // left eye
-    ellipse(200, 180, 20);  // right eye
+    ellipse(250, 570, 20, 35);  // left eye
+    ellipse(200, 570, 20, 35);  // right eye
     
     // second character
     fill('#843c23');
     noStroke();
-    arc(500, 200, 200, 200, 0, PI + QUARTER_PI, CHORD); //body
+    arc(500, 590, 200, 200, 0, PI + QUARTER_PI, CHORD); //body
     stroke(0);
-    arc(500, 200, 20, 20, 0, PI, CHORD); //right eye
-    arc(450, 200, 20, 20, 0, PI, CHORD); //left eye
+    arc(500, 570, 20, 27, 0, PI, CHORD); //right eye
+    arc(450, 570, 20, 27, 0, PI, CHORD); //left eye
     
+     //wall
+     fill('#564644');
+    noStroke();
+    rect(800, 200, 100, 2000);
+   
+    //ground
+    fill('#604130');
+    noStroke();
+    rect(0, 700, 1000, 2000);
+    
+    //grass
+    fill('#308744');
+    noStroke();
+    rect(0, 700, 1000, 100);
 }
