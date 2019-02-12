2e772cc3c835250d7dbcbd856c3ff0dc7fba8364
guywithpen <abreujaysonschool@gmail.com>
2019-02-11 19:02:58 -0500

    added to beginning

--- story/sketch.js
+++ story/sketch.js
@@ -4,7 +4,7 @@
 
 // global variable
 
-//beginning, middle, end
+//beginning, endone, endtwo
 var currentSetting = "beginning";
 
 function setup() {
@@ -13,6 +13,11 @@ function setup() {
 
 function draw() {
     background(50);
+    
+    // to change the scene
+		if (mouseIsPressed) {
+			currentSetting = "endone";
+		}
     
     //sky
     fill('#aec4e5');
@@ -28,7 +33,7 @@ function draw() {
     ellipse(200, 570, 20, 35);  // right eye
     
     // second character
-    fill('#843c23');
+    fill('#8c5269');
     noStroke();
     arc(500, 590, 200, 200, 0, PI + QUARTER_PI, CHORD); //body
     stroke(0);
@@ -36,7 +41,7 @@ function draw() {
     arc(450, 570, 20, 27, 0, PI, CHORD); //left eye
     
      //wall
-     fill('#564644');
+     fill('#422222');
     noStroke();
     rect(800, 200, 100, 2000);
    
@@ -49,4 +54,28 @@ function draw() {
     fill('#308744');
     noStroke();
     rect(0, 700, 1000, 100);
+    
+    //bubbleone
+       fill('white');
+    noStroke();
+    ellipse(200, 400, 160, 100);
+    ellipse(270, 440, 80, 50);
+    ellipse(270, 470, 40, 25);
+    fill('#3a3a3a');
+    rect(165, 365, 80, 40);
+    fill('#7a671d');
+    rect(196, 404, 17, 40);
+    
+    //bubbletwo
+       fill('white');
+    noStroke();
+    ellipse(600, 460, 160, 100);
+    ellipse(540, 500, 80, 50);
+    ellipse(540, 530, 40, 25);
+    fill('#9e2929');
+    rect(570, 430, 60, 25);
+    fill('#bababa');
+    rect(575, 470, 50, 8);
+    rect(575, 460, 50, 8);
+    
 }
