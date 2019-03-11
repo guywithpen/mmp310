70cc3f3b7afeece2be630f5979af7f8087250d0a
guywithpen <abreujaysonschool@gmail.com>
2019-03-11 18:46:03 -0400

    added code

--- save_image_loop/sketch.js
+++ save_image_loop/sketch.js
@@ -1,25 +1,29 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(51);
-	textSize(50);
-	
-	var columns = 6;
-	var rows = 4;
-	var w = width / columns; // column width
-	var h = height / rows; // row height
-	
-	for (let x = 0; x <= width; x += w) {
-		for (let y = 0; y <= height; y += h) {
+    createCanvas(windowWidth, windowHeight);
+    background(51);
+    textSize(50);
+
+    var columns = 6;
+    var rows = 4;
+    var w = width / columns; // column width
+    var h = height / rows; // row height
+
+    for (let x = 0; x <= width; x += w) {
+        for (let y = 0; y <= height; y += h) {
             noFill();
             stroke(255);
-            var = random(3);
+            var r = random(3);
             if (r > 2) {
-            bezier(x, y, x + w, y, x, y + h, x + w, y + h);
+                bezier(x, y, x + w, y, x, y + h, x + w, y + h);
+                ellipse(x + w, y, 15);
+                ellipse(x, y + h, 25);
+                ellipse(x, y, 35);
+                ellipse(x + w, y + h, 45);
             } else if (r > 1) {
-             bezier(x, y, x + w, y, x, y + h, x + w, y + h);
+                bezier(x, y + 6, x + w, y, x, y + h, x + w, y + h);
             } else {
-            bezier(x, y, x + w, y, x, y + h, x + w, y + h);
+                bezier(x, y, x + w, y, x, y + h, x + w, y + h);
             }
-		
-	}
-}
+
+        }
+    }
