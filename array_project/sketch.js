dbd4344c740772a3fe8d532bf4e9eca461abf204
guywithpen <abreujaysonschool@gmail.com>
2019-03-25 19:32:25 -0400

    new code

--- array_project/sketch.js
+++ array_project/sketch.js
@@ -1,34 +1,68 @@
+var points = []; // array to save xy points
+var rot = 0; // rotation variable
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(51);
-    rectMode(CENTER);
+    createCanvas(640, 360);
     noStroke();
+    fill('plum');
+}
 
-    for (let x = 0; x < width; x += 300) {
+function draw() {
+    background(51);
+
+    if (mouseIsPressed) {
         var r = random(0, 300);
-		var g = random(0, 500);
-		var b = random(100, 155);
-        var y = random(height/2 + 80, height/2 - 80);
-        fill(r,b,g);
-        ellipse(x, y -50, 200); // face
-        //fill(0);
-        //ellipse(x + 50, 50, 50); // left eyeS
-        //ellipse(x - 50, 50, 50); // right eye
-        fill('#a5639c');
-        triangle(x -60, y+3, x + 60, y+3, x + 1, y+20); // mouth
-        arc(x - 40, y-80, 50, 100, 0, PI + QUARTER_PI, CHORD);
-        arc(x + 40, y-80, 50, 100, 6.6 -QUARTER_PI, PI, CHORD);
-
+        var g = random(0, 500);
+        var b = random(100, 155);
+        var c = color(r, g, b);
+        var point = [mouseX, mouseY, c];
+        points.push(point);
+        
     }
 
-    let x = width / 10;
-    for (let i = 0; i < 5; i++) {
-        var c = map(i, 0, 4, 0, 255); // map color
-        fill(c);
-        var s = map(i, 0, 4, 50, 200); // map size
-        var y = map(i, 0, 4, 300, 500);
-        //ellipse(x, y, s);
-        x += width / 5;
+    for (let i = 0; i < points.length; i++) {
+        let x = points[i][0];
+        let y = points[i][1];
+        let c = points[i][2];
+
+        face(x, y, c);
+
+        points[i][1]++; // increment y by 1
     }
+    
+    	translate(width/2, height/2);
+	rotate(rot);
+	rot += 0.01;
+
+	for (let i = 0; i < points.length; i++) {
+		let x = points[i][0] - width/2; // offset the drawing 
+		let y = points[i][1] - height/2; 
+		    fill(c);
+    ellipse(x, y - 50, 200); // face
+    //fill(0);
+    //ellipse(x + 50, 50, 50); // left eyeS
+    //ellipse(x - 50, 50, 50); // right eye
+    fill('#a5639c');
+    triangle(x - 60, y + 3, x + 60, y + 3, x + 1, y + 20); // mouth
+    arc(x - 40, y - 80, 50, 100, 0, PI + QUARTER_PI, CHORD);
+    arc(x + 40, y - 80, 50, 100, 6.6 - QUARTER_PI, PI, CHORD);
+	}
+
+}
+
+function face(x, y, c) {
+    //var y = random(height/2 + 80, height/2 - 80);
+    fill(c);
+    ellipse(x, y - 50, 200); // face
+    //fill(0);
+    //ellipse(x + 50, 50, 50); // left eyeS
+    //ellipse(x - 50, 50, 50); // right eye
+    fill('#a5639c');
+    triangle(x - 60, y + 3, x + 60, y + 3, x + 1, y + 20); // mouth
+    arc(x - 40, y - 80, 50, 100, 0, PI + QUARTER_PI, CHORD);
+    arc(x + 40, y - 80, 50, 100, 6.6 - QUARTER_PI, PI, CHORD);
+    
+    
+    
+    
 
 }
