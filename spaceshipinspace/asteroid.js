cbf2c471f1856622f3cf560e56ea9eb5e00b1908
guywithpen <abreujaysonschool@gmail.com>
2019-04-08 17:35:11 -0400

    added more code

--- spaceshipinspace/asteroid.js
+++ spaceshipinspace/asteroid.js
@@ -1,10 +1,33 @@
 class Asteroid {
-    constructor() {
-        this.x = random(width);
-        this.y = -100;
-        this.size = 100;
-        this.speed = {
-            
-        };
-    };
+	constructor() {
+		this.x = random(width);
+		this.y = -100;
+		this.size = 100;
+		this.speed = {
+			x: random(-1, 1),
+			y: random(5)
+		};
+	}
+	
+	display() {
+		fill(0);
+		noStroke();
+		ellipse(this.x, this.y, this.size);
+	}
+	
+	update() {
+		this.x += this.speed.x;
+		this.y += this.speed.y;
+	}
+	
+	collide() {
+		var d = dist(this.x, this.y, spaceship.x, spaceship.y);
+		if (d < this.size / 2) {
+			textSize(100);
+			textAlign(CENTER, CENTER);
+			fill('red');
+			text("F A L L E N", width/2, height/2);
+			noLoop();	
+		}
+	}
 }
--- spaceshipinspace/sketch.js
+++ spaceshipinspace/sketch.js
@@ -1,30 +1,50 @@
-var spaceship = {
-	x: 0,
+
+var spaceship = { 
+	x: 0, 
 	y: 200,
-	size: 100,
+	size: 50,
 	speed: 5,
-	display: function() {
-		fill('white');
-		noStroke();
-		triangle(this.x, this.y, this.x + this.size/2, this.y + this.size, this.x - this.size/2, this.y + this.size);
+	display: function () {
+		//fill('pink');
+		<img src="sprite.png" alt="The Slayer">;
 	},
-	update: function() {
-		if (keyIsDown(LEFT_ARROW)) {
-			this.x -= thisthisthis.speed;
-		}
+	update: function () {
 		if (keyIsDown(RIGHT_ARROW)) {
-			this.x += thisthis.speed;
+			this.x += this.speed;
+		}
+	
+		if (keyIsDown(LEFT_ARROW)) {
+			this.x -= this.speed;
 		}
 	}
 };
 
+var asteroids = [];
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	noStroke();
+	
+	// position the spaceship at start of game
+	spaceship.x = width/2;
+	spaceship.y = height - 200;
+}
+
 function draw() {
 	background(51);
-
+	
+	// adds random asteroid
+	if (random(100) > 99) {
+		// create an asteroid
+		asteroids.push(new Asteroid());
+	}
+	
 	spaceship.display();
 	spaceship.update();
-    
-    for (let i = 0; i < asteroids. length; i++) {
-        asteroids[i].display();
-    }
+	
+	for (let i = 0; i < asteroids.length; i++) {
+		asteroids[i].display();
+		asteroids[i].update();
+		asteroids[i].collide();
+	}
 }
